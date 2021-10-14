8e4e8ea210dd2fd37be42b6b23fb2b6d8f3c2f1d
Pavel Polonchuk <chelonpepk@gmail.com>
2021-10-14 15:09:38 +0300

    Добавлена отсылка данных в InfluxDB на localhost.

--- webserver_influx.py
+++ webserver_influx.py
@@ -14,11 +14,11 @@ def start_server(q, serverHost, serverPort):
         def do_GET(self):
 
             if q.empty() == True:
-                result=1
+                result = 1
                 q.put(result)
             else:
                 result = q.get()
-                result+=1
+                result += 1
                 q.put(result)
 
             self.send_response(200)
@@ -47,6 +47,7 @@ def start_server(q, serverHost, serverPort):
         # print(f'Sum: {query_values_sum}')
 
     httpd = HTTPServer((serverHost, serverPort), MathServer)
+    print(f'Math server listening on http://{serverHost}:{serverPort}\n')
 
     try:
         httpd.serve_forever()
@@ -59,18 +60,42 @@ def start_server(q, serverHost, serverPort):
 
 def report_to_influxdb(q, dbHost, dbPort):
 
-    # start_time = datetime.now()
+    client = InfluxDBClient(dbHost, dbPort, 'math_server', 'math_server1', 'math_server')
+    client.create_database('math_server')
+    client.switch_database('math_server')
+    requests_reported = 0
+
     try:
         while True:
             time.sleep(60)
 
-            if q.empty() == True:
-                print('Queue is empty')
+            if q.empty() == False:
+                
+                requests_total = q.get()
+                q.put(requests_total)
+                requests_to_report = requests_total - requests_reported
             else:
-                result = q.get()
-                q.put(result)
-                print(f"Number of GET-requests to MathServer:{result}")
-
+                requests_total = 0
+                requests_to_report = 0
+            
+            report = [{
+                "measurement": "requests",
+                "tags": {
+                    "request": "GET_per_1_minute" 
+                    },
+                "time": datetime.now(),
+                "fields": {
+                    'requests_number': requests_to_report,
+                }
+            }]
+            
+            try:
+                client.write_points(report)
+                print(f'Report sent: Requests per time period: {requests_to_report}, requests from the start: {requests_total}')
+                requests_reported = requests_reported + requests_to_report
+            except:
+                print(f'Report was NOT sent at: {datetime.now()}')
+                
     except KeyboardInterrupt:
         pass
     print('DB report stopped.')
@@ -84,8 +109,6 @@ if __name__ == '__main__':
     dbPort = 8086
 
     q = Queue()
-
-    print(f'Listening on http://{serverHost}:{serverPort}\n')
 
     running_server = Process(target=start_server, args=(q, serverHost, serverPort))
     running_db = Process(target=report_to_influxdb, args=(q, dbHost, dbPort))
